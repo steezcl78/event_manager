0300e1e85ce454aced43dcbc9c96f3743b5f3510
cl78 <cl78@njit.edu>
2024-11-27 09:10:48 -0500

    fixed nickname typo in user_service

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -60,10 +60,10 @@ class UserService:
             validated_data['hashed_password'] = hash_password(validated_data.pop('password'))
             new_user = User(**validated_data)
             new_user.verification_token = generate_verification_token()
-            new_nickname = generate_nickname()
-            while await cls.get_by_nickname(session, new_nickname):
-                new_nickname = generate_nickname()
-            new_user.nickname = new_nickname
+            #new_nickname = generate_nickname()
+            #while await cls.get_by_nickname(session, new_nickname):
+             #new_nickname = generate_nickname()
+            #new_user.nickname = new_nickname
             session.add(new_user)
             await session.commit()
             await email_service.send_verification_email(new_user)
